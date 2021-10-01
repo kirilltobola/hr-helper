879d3e1ac0e289978a06970c1805223cf74caeaa
zindmax <s1ndbad1337@gmail.com>
2021-10-01 23:18:15 +0800

    delete unused namespaces in web.route

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Position;
-use App\Models\ProgrammingLevel;
-use App\Models\Candidate;
-use App\Models\Status;
-use \App\Models\Cv;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
