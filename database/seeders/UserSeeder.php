af4d35e0442c054b05c8981b032909714219e3ce
zindmax <s1ndbad1337@gmail.com>
2021-10-02 15:14:39 +0800

    Add DB seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'user',
+                'email' => 'user@mail.com',
+                'password' => Hash::make('user'),
+            ],
+        ]);
+
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@mail.com',
+                'is_admin' => true,
+                'password' => Hash::make('admin'),
+            ],
+        ]);
+    }
+}
