7dd46ab3fbdbb76979747fab3c40189b44e3c47c
Kirill Tobola <kirilltobola@gmail.com>
2021-10-09 18:59:54 +0800

    Refactor admin views names

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts')
+@section('content')
+    <form action="{{ route('admin.store', ['model' => $modelAlias]) }}" method="post">
+        @csrf
+        @foreach($attributes as $attribute)
+            <div class="form-floating mb-3">
+                <input class="form-control" id="{{ $attribute }}" name="{{ $attribute }}" type="text" value="{{old($attribute)}}">
+                <label for="{{$attribute}}">{{ $attribute }}</label>
+                @error($attribute)
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+        @endforeach
+        <button class="btn btn-dark" type="submit">Добавить</button>
+    </form>
+@endsection
